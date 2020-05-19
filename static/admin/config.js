f3061014d793f8d7b882a7807e8da34281eb6405
Alex Earnshaw <aearnshaw@axway.com>
2020-05-19 14:38:36 +0100

    Test getting the branch for the CMS from Netlify build

--- static/admin/config.js
+++ static/admin/config.js
@@ -392,14 +392,14 @@ const collections = [{
   folder: 'content/en/blog/friends',
 }];
 
-window.MY_CMS_BRANCH = process.env.BRANCH;
+const { BRANCH } = process.env;
 
 const config = {
   backend: {
     name: 'github',
     repo: 'Axway/axway-open-docs', //Path to your GitHub repository. For fork testing use alexearnshaw/axway-open-docs.
     open_authoring: true,
-    branch: window.MY_CMS_BRANCH || "master",
+    branch: BRANCH || "master",
   },
   publish_mode: 'editorial_workflow',
   media_folder: '/static/Images', // Media files will be stored in the repo under static/Images
